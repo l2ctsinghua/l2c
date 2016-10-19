c5b20997bf3f2063d9d270d3787b470af46ca49d
oops <shangshu1992@gmail.com>
2016-10-19 15:29:31 +0800

    remove useless files

--- test/test.sh
+++ test/test.sh
@@ -10,14 +10,11 @@ fi
 files=`find ./cases/ -name "*.ast"`
 for f in $files
 do
-    if [[ $f =~ ^\./cases/([^\.]+)\.ast$ ]]
-    then
-	echo $f;
-	mkdir result/${BASH_REMATCH[1]};
-	../l2c -target_dir result/${BASH_REMATCH[1]} $f;
-    else
-	echo "Not propper format";
-    fi
+    name=`echo $f | cut -d'/' -f 3 | cut -d'.' -f 1`;
+    echo $f;
+    rm -r result/$name;
+    mkdir result/$name;
+    ../l2c -target_dir result/$name $f;
 done
 
 cd ..
